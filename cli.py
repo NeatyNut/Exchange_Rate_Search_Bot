4422b2515b5626c67a7aefbe62b89a9c53cb7e5e
Mingi Kim <89675001+NeatyNut@users.noreply.github.com>
2025-04-09 00:14:46 +0900

    Update cli.py

--- cli.py
+++ cli.py
@@ -6,5 +6,5 @@ import time
 while True:
     date, times, price, updown, percent = get_currency()
     insert_row(date, times, price, updown, percent)
-    send_message(f"{date} {times} 기준, 엔화 가격은 {price}로 전일 대비 {updown}, {percent}")
+    send_message(f"{date} {times} 기준, 엔화 가격은 {price}원으로 전일 대비 {updown}, {percent}")
     time.sleep(1800)
