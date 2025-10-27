af380b62aa24c47bda5bb151f9ed9dd32db0e96a
NeatyNut <uilove216@gmail.com>
2025-10-28 00:25:32 +0900

    timefix

--- cli.py
+++ cli.py
@@ -1,8 +1,9 @@
 # from JPYKRW import get_currency
 # from googlesheet import insert_row
 from bot import bot
-from datetime import datetime, time
+import datetime
 import scheduler
+import time
 
 if __name__ == "__main__":
     while True:
@@ -10,8 +11,8 @@ if __name__ == "__main__":
         # insert_row(date, times, price, updown, percent)
         # send_message(f"{date} {times} 기준, 대만TWD 가격은 {price}원으로 전일 대비 {updown}, {percent}")
         
-        now = datetime.now().time()
-        if time(2, 0) < now < time(9, 0):
+        now = datetime.datetime.now().time()
+        if datetime.time(2, 0) < now < datetime.time(9, 0):
             sleep_seconds = scheduler.waiting()
             time.sleep(sleep_seconds)
             continue
